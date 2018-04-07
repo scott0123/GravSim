59adb05b22a59cf5cc2cef3793df7364029244fe
Scott <Scott@Scott-Liu.com>
2018-04-07 01:05:21 -0500

    corrections to force calculation. (force is a vector, not just a scalar) also added function headers

--- code/main.c
+++ code/main.c
@@ -1,20 +1,29 @@
-/************************************************************************
+/***********************************************************************************
+
+            ██████╗ ██████╗  █████╗ ██╗   ██╗███████╗██╗███╗   ███╗
+           ██╔════╝ ██╔══██╗██╔══██╗██║   ██║██╔════╝██║████╗ ████║
+           ██║  ███╗██████╔╝███████║██║   ██║███████╗██║██╔████╔██║
+           ██║   ██║██╔══██╗██╔══██║╚██╗ ██╔╝╚════██║██║██║╚██╔╝██║
+           ╚██████╔╝██║  ██║██║  ██║ ╚████╔╝ ███████║██║██║ ╚═╝ ██║
+            ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝╚═╝     ╚═╝
+
 GravSim main file
 
 Scott Liu, Spring 2018
 
 For use in
 
- ███████╗ ██████╗███████╗██████╗  █████╗ ███████╗
- ██╔════╝██╔════╝██╔════╝╚════██╗██╔══██╗██╔════╝
- █████╗  ██║     █████╗   █████╔╝╚█████╔╝███████╗
- ██╔══╝  ██║     ██╔══╝   ╚═══██╗██╔══██╗╚════██║
- ███████╗╚██████╗███████╗██████╔╝╚█████╔╝███████║
- ╚══════╝ ╚═════╝╚══════╝╚═════╝  ╚════╝ ╚══════╝
+             ███████╗ ██████╗███████╗██████╗  █████╗ ███████╗
+             ██╔════╝██╔════╝██╔════╝╚════██╗██╔══██╗██╔════╝
+             █████╗  ██║     █████╗   █████╔╝╚█████╔╝███████╗
+             ██╔══╝  ██║     ██╔══╝   ╚═══██╗██╔══██╗╚════██║
+             ███████╗╚██████╗███████╗██████╔╝╚█████╔╝███████║
+             ╚══════╝ ╚═════╝╚══════╝╚═════╝  ╚════╝ ╚══════╝
  
 University of Illinois Electrical & Computer Engineering Department
 (This is used to prevent a simple Google search giving free code to future students)
-************************************************************************/
+
+************************************************************************************/
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -29,16 +38,16 @@ void debug();
 void software_simulation();
 void hardware_simulation();
 
-float get_force_between_planets(planet a, planet b);
+force get_force_between_planets(planet a, planet b);
 
 // Pointer to base address of GravSim Hardware memory,
 // this needs to exactly match the address in Qsys
 volatile unsigned int * MEM_PTR = (unsigned int *) 0x00001000;
 
-/*  main()
+/* 
+ *  main()
  *
- *  Execute the pre-defined method of simulation
- *
+ *  Execute the simulation type of choice
  */
 int main(){
 
@@ -63,12 +72,17 @@ void debug(){
     
     planet p1 = { 1000000.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0 };
     planet p2 = { 1000000.0, 1.0, -1.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0 };
-    float force = get_force_between_planets(p1, p2);
-    printf("we think the force is %.2fN\n", force);
+    force f = get_force_between_planets(p1, p2);
+    printf("Planet 1 is at (%.2f, %.2f, %.2f)\n", p1.pos_x, p1.pos_y, p1.pos_z);
+    printf("Planet 2 is at (%.2f, %.2f, %.2f)\n", p2.pos_x, p2.pos_y, p2.pos_z);
+    printf("This force has magnitude %.2fN\n", f.magnitude);
+    printf("This force is in direction (%.2f, %.2f, %.2f)\n", f.dir_x, f.dir_y, f.dir_z);
 }
 
-/* software_simulation()
+/* 
+ * software_simulation()
  *
+ * This function handles the entirety of the software simulation
  */
 void software_simulation(){
     
@@ -91,20 +105,34 @@ void software_simulation(){
     
 }
 
-/* hardware_simulation()
+/* 
+ * hardware_simulation()
  *
+ * This function handles the entirety of the hardware simulation
  */
 void hardware_simulation(){
     // TODO
 }
 
 
-float get_force_between_planets(planet a, planet b){
+/*
+ *  get_force_between_planets(planet a, planet b)
+ *
+ *  The resulting force is the force planet B applies to planet A
+ */
+force get_force_between_planets(planet a, planet b){
 
     float rel_pos_squared = fabs(a.pos_x - b.pos_x) * fabs(a.pos_x - b.pos_x)
                           + fabs(a.pos_y - b.pos_y) * fabs(a.pos_y - b.pos_y)
                           + fabs(a.pos_z - b.pos_z) * fabs(a.pos_z - b.pos_z);
+    
+    // direction needs to be normallized
+    float mag = sqrt(rel_pos_squared);
+    float dir_x = (b.pos_x - a.pos_x) / mag;
+    float dir_y = (b.pos_y - a.pos_y) / mag;
+    float dir_z = (b.pos_z - a.pos_z) / mag;
 
-    return G * a.mass * b.mass / rel_pos_squared;
+    force f = {G * a.mass * b.mass / rel_pos_squared, dir_x, dir_y, dir_z};
+    return f;
 }
 
