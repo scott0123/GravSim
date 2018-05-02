a1cf4f4809194af1fe342d77d2c9b61c07424672
Scott <Scott@Scott-Liu.com>
2018-05-02 00:05:57 -0500

    created a new main file that now support keyboard input

---
+++ SV/software_codes/keyboard.h
@@ -0,0 +1,23 @@
+#ifndef _KEYBOARD_H
+#define _KEYBOARD_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <io.h>
+#include <fcntl.h>
+
+#include "system.h"
+#include "alt_types.h"
+#include <unistd.h>  // usleep 
+#include "sys/alt_irq.h"
+#include "io_handler.h"
+
+#include "cy7c67200.h"
+#include "usb.h"
+#include "lcp_cmd.h"
+#include "lcp_data.h"
+
+int keyboard_main(void);
+
+#endif
